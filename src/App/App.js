7cc060b0181045905ddc785e04fd5d534fd81c99
Maggie Greene <maggie.greene96@gmail.com>
2020-01-08 20:05:30 -0600

    Work in progress

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import firebaseConnection from '../helpers/data/connection';
+
 import './App.scss';
 
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+firebaseConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <button className="btn btn-danger">Bootstrap Button</button>
+        <Router>
+          <Switch>
+            <PublicRoute path="/" exact authed={authed} />
+            <PrivateRoute />
+            <PrivateRoute />
+            <PrivateRoute />
+          </Switch>
+        </Router>
       </div>
     );
   }
